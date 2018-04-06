ed3f572d0886adba18cbcd1e44358d8bb0926f87
Julien Marchand <julien.marchand4@gmail.com>
2018-04-06 12:58:47 +0200

    First commit empty repo

---
+++ truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "5777",
+            gasPrice: 1,
+        }
+    }
+};
