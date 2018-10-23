71a43061c4e77b7e76538342b0d57fffa6aedc74
Julien Marchand <julien.marchand4@gmail.com>
2018-10-23 15:26:25 +0200

    feat(provider): add custom dot env provider

--- truffle-config.js
+++ truffle-config.js
@@ -39,5 +39,12 @@ module.exports = {
       port: 7545,
       network_id: '*', // eslint-disable-line camelcase
     },
+    dotEnvNetwork: {
+      provider: providerWithMnemonic(
+        process.env.MNEMONIC,
+        process.env.RPC_ENDPOINT
+      ),
+      network_id: parseInt(process.env.NETWORK_ID) || '*', // eslint-disable-line camelcase
+    },
   },
 };
