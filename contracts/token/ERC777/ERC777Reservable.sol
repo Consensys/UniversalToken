3596569d85cf4ae07ba30f1788612c4fa49b2eeb
Julien Marchand <julien.marchand4@gmail.com>
2018-10-25 16:56:05 +0200

    feat(erc777-reservable): add minShares maxShares and burnLeftOver mechanisms

--- contracts/token/ERC777/ERC777Reservable.sol
+++ contracts/token/ERC777/ERC777Reservable.sol
@@ -5,90 +5,119 @@
 pragma solidity ^0.4.24;
 
 import { Ownable as ozs_Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 import "contract-certificate-controller/contracts/Controllable.sol";
 
 import "./ERC777.sol";
 
 
-contract ERC777Reservable is ozs_Ownable, MinterRole, Controllable, ERC777 {
-    uint256 public _minShares;
-    uint256 public _burnLeftOver;
+contract ERC777Reservable is ozs_Ownable, Controllable, ERC777 {
+  using SafeMath for uint256;
+  
+  uint256 public _minShares;
+  bool public _burnLeftOver;
+  bool public _saleEnded;
+  uint256 public _reservedTotal;
+  uint256 public _validatedTotal;
 
-    enum Status { Created, Validated, Cancelled }
+  enum Status { Created, Validated, Cancelled }
 
-    struct Reservation {
-        Status status;
-        uint256 amount;
-        uint256 validUntil;
+  struct Reservation {
+    Status status;
+    uint256 amount;
+    uint256 validUntil;
+  }
+
+  mapping(address => Reservation[]) public _reservations;
+
+  struct ReservationCoordinates { address owner; uint256 index; }
+  ReservationCoordinates[] _validatedReservations;
+
+  event TokensReserved(address, uint256, uint256);
+  event ReservationValidated(address, uint256);
+  event SaleEnded();
+
+  constructor(
+    string name,
+    string symbol,
+    uint256 granularity,
+    address[] defaultOperators,
+    uint256 minShares,
+    uint256 maxShares,
+    bool burnLeftOver
+  )
+  public
+  ERC777(name, symbol, granularity, defaultOperators)
+  {
+    _minShares = minShares;
+    _burnLeftOver = burnLeftOver;
+    _balances[this] = maxShares;
+    _totalSupply = maxShares;
+  }
+
+  modifier onlySale() {
+    require(!_saleEnded, "0x45: Sale no longer available");
+    _;
+  }
+
+  function _reserve(uint256 amount, uint256 validUntil)
+  internal
+  onlySale
+  returns (uint256 index)
+  {
+    require(amount != 0, "0xA0: Amount should not be 0");
+
+    index = _reservations[msg.sender].push(Reservation(Status.Created, amount, validUntil));
+    _reservedTotal = _reservedTotal.add(amount);
+
+    require(_reservedTotal <= _totalSupply, "0xA0: The total reserved exceeds the total supply");
+
+    emit TokensReserved(msg.sender, index, amount);
+  }
+
+  function validateReservation(address owner, uint8 index)
+  public
+  onlyOwner
+  onlySale
+  {
+    require(_reservations[owner].length > 0 || _reservations[owner][index].status == Status.Created, "0x20: Invalid reservation");
+
+    Reservation storage reservation = _reservations[owner][index];
+    require(reservation.validUntil != 0 && reservation.validUntil < block.number, "0x05: Reservation has expired");
+
+    reservation.status = Status.Validated;
+    _validatedReservations.push(ReservationCoordinates(owner, index));
+
+    _validatedTotal = _validatedTotal.add(reservation.amount);
+
+    emit ReservationValidated(owner, index);
+  }
+
+  function endSale() 
+  public
+  onlySale
+  {
+    require(_minShares < _validatedTotal, "0xA0: The minimum validated has been reached");
+
+    for (uint256 i = 0; i < _validatedReservations.length; i++) {
+      Reservation storage reservation = _reservations[_validatedReservations[i].owner][_validatedReservations[i].index];
+      ERC777._send(this, this, _validatedReservations[i].owner, reservation.amount, "", "", true);
     }
 
-    mapping(address => Reservation[]) public reservations;
-
-    struct ReservationCoordinates { address owner; uint256 index; }
-    ReservationCoordinates[] validatedReservations;
-
-    event TokensReserved(address, uint256, uint256);
-    event ReservationValidated(address, uint256);
-    event SaleEnded();
-
-    constructor(
-        string name,
-        string symbol,
-        uint256 granularity,
-        address[] defaultOperators,
-        uint256 minShares,
-        uint256 maxShares,
-        uint256 burnLeftOver
-    )
-    public
-    ERC777(name, symbol, granularity, defaultOperators)
-    {
-        _minShares = minShares;
-        _burnLeftOver = burnLeftOver;
-        _balances[this] = maxShares;
-        _totalSupply = maxShares;
+    if (_burnLeftOver) {
+      ERC777._burn(this, this, _balances[this], "");
     }
 
-    function _reserve(uint256 amount, uint256 validUntil)
-    internal
-    returns (uint256 index)
-    {
-        require(amount != 0, "0xA0: Amount should not be 0");
+    _saleEnded = true;
 
-        index = reservations[msg.sender].push(Reservation(Status.Created, amount, validUntil));
+    emit SaleEnded();
+  }
 
-        emit TokensReserved(msg.sender, index, amount);
-    }
-
-    function validateReservation(address owner, uint8 index)
-    public
-    onlyOwner
-    {
-        require(reservations[owner].length > 0 || reservations[owner][index].status == Status.Created, "0x20: Invalid reservation");
-
-        Reservation storage reservation = reservations[owner][index];
-        require(reservation.validUntil != 0 && reservation.validUntil < block.number, "0x05: Reservation has expired");
-
-        reservation.status = Status.Validated;
-        validatedReservations.push(ReservationCoordinates(owner, index));
-
-        emit ReservationValidated(owner, index);
-    }
-
-    function endSale() public {
-        for (uint256 i = 0; i < validatedReservations.length; i++) {
-            Reservation storage reservation = reservations[validatedReservations[i].owner][validatedReservations[i].index];
-            ERC777._send(this, this, validatedReservations[i].owner, reservation.amount, "", "", true);
-        }
-        emit SaleEnded();
-    }
-
-    function()
-    public
-    isValid()
-    {
-        _reserve(1, 0);
-    }
+  function()
+  public
+  isValid()
+  {
+    _reserve(1, 0);
+  }
 }
