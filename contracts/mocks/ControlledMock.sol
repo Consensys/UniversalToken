b3d004695606a1780dfd9f50a824b713a7400516
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-10-31 11:20:33 +0100

    feature@mock(ControlledMock): update mock test method to prove compatibility with dynamic arguments

--- contracts/mocks/ControlledMock.sol
+++ contracts/mocks/ControlledMock.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "../CertificateController.sol";
- 
+
 
 /**
  * @title ControlledMock
@@ -9,12 +9,19 @@ import "../CertificateController.sol";
  */
 contract ControlledMock is CertificateController {
     
+    constructor(
+        address _certificateSigner   
+    )
+        public
+        CertificateController(_certificateSigner)
+    {}
+    
     /**
      * @dev A test function
      * @param i An integer
      * @param data Certificate to control
      */
-    function test(uint i, bytes data) 
+    function test(uint i, bytes b, bytes data) 
         public
         payable         
         isValidCertificate(data) 
