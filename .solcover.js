7c8413eafa1fb674a39629e9c1c875f40318d449
Julien Marchand <julien.marchand4@gmail.com>
2018-11-02 14:38:32 +0100

    fix(coverage): use builtin test-rpc for coverage instead of docker

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,4 @@
 module.exports = {
-    norpc: !process.env.CI,
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
     skipFiles: [
