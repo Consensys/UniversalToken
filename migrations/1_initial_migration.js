e65f28bab93a4cd016741e9f0217ca065b5e5bb5
Gauthier Petetin <gauthierpetetin@hotmail.com>
2020-02-22 18:46:05 +0100

    feat(migration): Add extension deployment in mogrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-var Migrations = artifacts.require('./Migrations.sol');
+const Migrations = artifacts.require('./Migrations.sol');
 
 module.exports = function (deployer, network, accounts) {
   console.log('Account to load with ETH: ', accounts[0]);
