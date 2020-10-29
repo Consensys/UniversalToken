b4f65690badd740963a0b6a4b2ec0d336ba30069
gauthierpetetin <gauthier.petetin@hotmail.com>
2020-10-29 10:51:07 +0100

    fix(erc20 and erc721): tokens shall be compatible with erc1820 registry

--- contracts/tokens/ERC20Token.sol
+++ contracts/tokens/ERC20Token.sol
@@ -7,8 +7,13 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract ERC20Token is Ownable, ERC20Mintable, ERC20Burnable, ERC20Pausable, ERC20Detailed {
+import "../interface/ERC1820Implementer.sol";
 
-  constructor(string memory name, string memory symbol, uint8 decimals) public ERC20Detailed(name, symbol, decimals) {}
+contract ERC20Token is Ownable, ERC20Mintable, ERC20Burnable, ERC20Pausable, ERC20Detailed, ERC1820Implementer {
+  string constant internal ERC20_TOKEN = "ERC20Token";
+
+  constructor(string memory name, string memory symbol, uint8 decimals) public ERC20Detailed(name, symbol, decimals) {
+    ERC1820Implementer._setInterface(ERC20_TOKEN);
+  }
 
 }
