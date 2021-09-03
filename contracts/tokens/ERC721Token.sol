15cb04dcc7bccf74b51e76cf7a0f9f29031e9c42
Eddie Penta <eddie@avalabs.org>
2021-09-03 09:25:53 -0400

    Use ERC721URIStorage contract to support optional baseURI

--- contracts/tokens/ERC721Token.sol
+++ contracts/tokens/ERC721Token.sol
@@ -6,16 +6,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "../interface/ERC1820Implementer.sol";
 import "../roles/MinterRole.sol";
 
-contract ERC721Token is Ownable, ERC721, ERC721Enumerable, ERC721Burnable, ERC721Pausable,  MinterRole, ERC1820Implementer, AccessControlEnumerable {
+contract ERC721Token is Ownable, ERC721, ERC721URIStorage, ERC721Enumerable, ERC721Burnable, ERC721Pausable,  MinterRole, ERC1820Implementer, AccessControlEnumerable {
   string constant internal ERC721_TOKEN = "ERC721Token";
-  mapping(uint256 => string) _tokenUris;
+  string internal _baseUri;
 
-  constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+  constructor(string memory name, string memory symbol, string memory baseUri) ERC721(name, symbol) {
     ERC1820Implementer._setInterface(ERC721_TOKEN);
+    _baseUri = baseUri;
   }
 
   /**
@@ -28,30 +30,13 @@ contract ERC721Token is Ownable, ERC721, ERC721Enumerable, ERC721Burnable, ERC72
       _mint(to, tokenId);
       return true;
   }
-  
-  /**
-  * @dev Function to set a URI for a given tokenId
-  * @param tokenId The tokenId to assign the given URI
-  * @param uri The URI to give the given tokenId
-  */
-  function setTokenUri(uint256 tokenId, string memory uri) external virtual onlyMinter returns (bool) {
-      require(_exists(tokenId), "ERC721Metadata: Setting URI for nonexistent token");
-      _tokenUris[tokenId] = uri;
-      return true;
+
+  function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+      return ERC721URIStorage.tokenURI(tokenId);
   }
 
-  /**
-    * @dev See {IERC721Metadata-tokenURI}.
-    */
-  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-      require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-
-      string memory uri = _tokenUris[tokenId];
-      if (bytes(uri).length > 0) {
-        return uri;
-      } else {
-        return super.tokenURI(tokenId);
-      }
+  function _baseURI() internal view override virtual returns (string memory) {
+      return _baseUri;
   }
 
   function _beforeTokenTransfer(
@@ -73,5 +58,9 @@ contract ERC721Token is Ownable, ERC721, ERC721Enumerable, ERC721Burnable, ERC72
       returns (bool)
   {
       return super.supportsInterface(interfaceId);
+  }
+
+  function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+    ERC721URIStorage._burn(tokenId);
   }
 }
