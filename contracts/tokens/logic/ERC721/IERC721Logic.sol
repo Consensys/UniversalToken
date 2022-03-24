70fdf601debc155e0e1b9e090edaef159d648d44
Eddie Penta <eddie.penta@consensys.net>
2022-03-24 11:07:40 -0400

    Remove ERC721 & ERC1155 from develop branch

--- contracts/tokens/logic/ERC721/IERC721Logic.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
-import {ITokenLogic} from "../ITokenLogic.sol";
-
-interface IERC721Logic is IERC721Metadata, ITokenLogic {
-    function burn(uint256 amount) external returns (bool);
-}
