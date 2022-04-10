3822ecc7223854ca9953a765d930d6a0f621b18d
Eddie Penta <eddie.penta@consensys.net>
2022-04-09 20:55:54 -0400

    possible fix for coverage

--- truffle-config.js
+++ truffle-config.js
@@ -44,11 +44,9 @@ module.exports = {
       gasPrice: 5000000000
     },
     coverage: {
-      host: 'localhost',
-      network_id: '*', // eslint-disable-line camelcase
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01,
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
       disableConfirmationListener: true,
     },
     ganache: {
