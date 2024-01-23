54320c6f7a8ee1fd7fcb19073e9c122e1e8f96f9
francesco.miliani <75777914+francescomiliani@users.noreply.github.com>
2024-01-23 10:19:18 +0100

    Update IERC20HoldableToken.sol (#168)
    Fix a typo

--- contracts/interface/IERC20HoldableToken.sol
+++ contracts/interface/IERC20HoldableToken.sol
@@ -43,7 +43,7 @@ interface IERC20HoldableToken is IERC20 {
      @notice Called by the sender to hold some tokens for a recipient that the sender can not release back to themself until after the expiration date.
      @param holdId a unique identifier for the hold.
      @param recipient optional account the tokens will be transferred to on execution. If a zero address, the recipient must be specified on execution of the hold.
-     @param notary account that can execute the hold. Typically the recipient but can be a third party or a smart contact.
+     @param notary account that can execute the hold. Typically the recipient but can be a third party or a smart contract.
      @param amount of tokens to be transferred to the recipient on execution. Must be a non zero amount.
      @param expirationDateTime UNIX epoch seconds the held amount can be released back to the sender by the sender. Past dates are allowed.
      @param lockHash optional keccak256 hash of a lock preimage. An empty hash will not enforce the hash lock when the hold is executed.
